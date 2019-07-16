fd5eebf463d9ff2e1e8882e99e03f9e7d8588bac
Daniel Salunga <dsalunga@live.com>
2019-07-16 17:03:05 +0800

    Package fixes, etc

--- Portal/_set-env-vars.cmd
+++ Portal/_set-env-vars.cmd
@@ -1,2 +1,7 @@
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
+) else (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+)
