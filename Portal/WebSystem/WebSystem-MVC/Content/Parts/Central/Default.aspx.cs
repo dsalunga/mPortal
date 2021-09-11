3ebb7ce5259547a223d398313731836f8d28d6d4
Daniel Salunga <dsalunga@live.com>
2021-09-11 18:35:50 +0800

    bug fixes

--- Portal/WebSystem/WebSystem-MVC/Content/Parts/Central/Default.aspx.cs
+++ Portal/WebSystem/WebSystem-MVC/Content/Parts/Central/Default.aspx.cs
@@ -54,7 +54,7 @@ namespace WCMS.WebSystem.WebParts.Central
                     var item = WebPageElement.Get(pageElementId);
                     if (item != null)
                     {
-                        title = "App Settings: " + item.Name;
+                        title = string.Format("{0} - App Settings" + item.Name);
 
                         try
                         {
@@ -98,7 +98,7 @@ namespace WCMS.WebSystem.WebParts.Central
                     var page = WPage.Get(pageId);
                     if (page != null)
                     {
-                        title = "App Settings: " + page.Name;
+                        title = string.Format("{0} - App Settings" + page.Name);
 
                         try
                         {
@@ -181,7 +181,7 @@ namespace WCMS.WebSystem.WebParts.Central
                 var partAdmin = WebPartAdmin.Get(partAdminId);
                 if (partAdmin != null)
                 {
-                    title = "App Administration: " + partAdmin.Name;
+                    title = string.Format("{0} - App Administration", partAdmin.Name);
 
                     try
                     {
